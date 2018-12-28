6283f4db83341531ae397fe7580f448a046c2f33
czy15036082899 <czy15036082899@163.com>
2018-12-28 09:20:34 +0800

    chen第一次提交

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,36 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//chen的提交
+//前台注册路由
+Route::resource('home/login/register','home\login\RegisterController');
+//激活账户路由
+Route::get('home/login/status/{id}/{token}','home\login\RegisterController@setstatus');
+//使用手机号注册
+Route::post('home/login/insert','home\login\RegisterController@insert');
+Route::get('home/login/sendMobileCode','home\login\RegisterController@sendMobileCode');
+Route::get('home/login/yanzheng/{phone}','home\login\RegisterController@yanzheng');
